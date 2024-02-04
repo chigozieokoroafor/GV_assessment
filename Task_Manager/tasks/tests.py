bab0a41a3d19005a27e5d0a662a93d37e08aacd3
chigozieokoroafor <okoroaforc14@gmail.com>
2024-02-04 18:22:12 +0100

    started writing test cases for views

--- Task_Manager/tasks/tests.py
+++ Task_Manager/tasks/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from tasks.views import my_task
 
 # Create your tests here.
+class TaskTestCase(TestCase):
+    def test_list_resolve(self):
+        # print(resolve(reverse("task_route")))
+        url =  reverse("task_route")
+        self.assertEquals(resolve(url).func, my_task)
